8576f6161c9e55e379d06bc2976a5bb0be955a71
Yordaign <Yordaign@Protonmail.com>
2024-11-03 22:49:47 +0000

    Changed format to allign with Boot.Dev

--- main.py
+++ main.py
@@ -1,7 +1,62 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-    print(file_contents)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(num_words)
 
-if __name__ == "__main__":
-    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  #  with open("books/frankenstein.txt") as f:
+  #      file_contents = f.read()
+  #  print(file_contents)
+#def word_count():
+#    with open("books/frankenstein.txt") as f:
+ #       file_contents = f.read()
+ #       words = file_contents.split()
+ #       count = len(words)
+ #       print(count)
+#if __name__ == "__main__":
+   # main()
+ #   word_count()
