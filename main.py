b89dce37d77b2139a76efecb956b331c46ee30e3
Yordaign <Yordaign@Protonmail.com>
2024-11-05 23:00:37 +0000

    Added book report function that prints off number of words and sorts character appearances based off frequency of use

--- main.py
+++ main.py
@@ -3,17 +3,23 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     character_count = count_char(text)
+    print(get_report(character_count, num_words, book_path))
 
+#currently coding the below: need to add a sort function to the list of dictionaries, and integrate them into the final report string <3
+#you got this king
 
+def sort_on(dict):
+    return dict["num"]
 
-
-
-
-
-
-
-
-
+def get_report(char_dict, num_words, book_path):
+    report = f"--- Begin report of {book_path} ---\n"
+    report += f"{num_words} words found in the document \n\n"
+    char_dict_list = [{"char": char, "num": count} for char, count in char_dict.items() if char.isalpha()]
+    char_dict_list.sort(reverse=True, key=sort_on)
+    for entry in char_dict_list:
+        report += f"The '{entry['char']}' character was found {entry['num']} times\n"
+    report += "--- End report ---"
+    return report
 
 def count_char(text):
     char_dict = {}
